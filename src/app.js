365aa21e5c21d93b06dec6e09624763733e29661
fl-bunya <yuichi.bunya@firstlogic.co.jp>
2023-11-24 18:54:28 +0900

    Refactor code to separate image generation logic into a separate module

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-const { App, AwsLambdaReceiver } = require('@slack/bolt');
-const OpenAI = require('openai');
-const { WebClient } = require('@slack/web-api');
 require('dotenv').config();
+const { App, AwsLambdaReceiver } = require('@slack/bolt');
+const { dalle } = require('./dalle');
 
 // Initialize your custom receiver
 const awsLambdaReceiver = new AwsLambdaReceiver({
@@ -14,79 +13,21 @@ const app = new App({
   receiver: awsLambdaReceiver,
 });
 
-// For uploading an image to Slack
-const client = new WebClient(process.env.SLACK_BOT_TOKEN);
-
 // Handle the Lambda function event
 module.exports.handler = async (event, context, callback) => {
   const handler = await awsLambdaReceiver.start();
   return handler(event, context, callback);
 }
 
-const fetchImgBase64GeneratedByAI = async (prompt) => {
-  const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const response = await openai.images.generate({
-    model: "dall-e-3",
-    prompt,
-    n: 1,
-    size: "1024x1024",
-    response_format: "b64_json",
-  });
-  const data = response.data[0];
-  return data;
-}
-
-function sanitizeFilename(input) {
-  // Replace invalid characters with underscore
-  return input.replace(/[\/\\?%*:|"<>]/g, '_');
-}
-
-async function uploadBase64Image(
-  b64_json,
-  prompt,
-  initial_comment,
-  channelId,
-  thread_ts = null
-) {
-  try {
-    const filename = sanitizeFilename(prompt) + '.jpg';
-
-    // Remove the "data:image/jpeg;base64," part from the base64 image string
-    const base64Data = b64_json.replace(/^data:image\/\w+;base64,/, '');
-    // Convert the base64 image to binary data
-    const binaryData = Buffer.from(base64Data, 'base64');
-
-    const result = await client.files.uploadV2({
-      file: binaryData,
-      filename,
-      initial_comment,
-      channels: channelId,
-      thread_ts,
-    });
-
-    console.log('File uploaded: ', JSON.stringify(result));
-  } catch (error) {
-    console.error('Error uploading file: ', error);
-  }
-}
 
 // Listen for direct messages
 app.message(async ({ message, say }) => {
   try {
-    // only respond to direct messages
-    const prompt = message.text;
-
-    if (message.channel_type === 'im' && !message.bot_id && prompt) {
-      await say(`Ok, I'll draw ... ${prompt}`);
-      const image = await fetchImgBase64GeneratedByAI(prompt);
-      const initial_comment = image.revised_prompt;
-      await uploadBase64Image(image.b64_json, prompt, initial_comment, message.channel);
-    }
+    if (message.channel_type !== 'im' || message.bot_id) return;
+    const { text: prompt, user, channel } = message;
+    await dalle(say, prompt, user, channel);
   } catch (error) {
     console.error('Error:', error);
-    // Respond with a message in case of error
     await say(`Sorry, an error occurred: ${error.message}`);
   }
 });
@@ -94,13 +35,8 @@ app.message(async ({ message, say }) => {
 app.event('app_mention', async ({ event, say }) => {
   try {
     const prompt = event.text.replace(/<@.*>/, '').trim();
-    await say({
-      text: `Ok, I'll draw ... ${prompt}`,
-      thread_ts: event.ts,  // Reply in thread
-    });
-    const image = await fetchImgBase64GeneratedByAI(prompt);
-    const initial_comment = `Done. <@${event.user}>\n${image.revised_prompt}`;
-    await uploadBase64Image(image.b64_json, prompt, initial_comment, event.channel, event.ts);
+    const { user, channel, ts: thread_ts } = event;
+    await dalle(say, prompt, user, channel, thread_ts);
   } catch (error) {
     console.error('Error:', error);
     await say({
